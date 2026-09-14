27c5b0e594d65373d8a64b7ca22f93d81f9af6ce
1NVI5IBLE <62433063+1NVI5IBLE@users.noreply.github.com>
2026-09-14 16:56:46 +0100

    Add dbt staging model for race results

---
+++ f1_dbt/models/example/staging/stg_race_reesults.sql
@@ -0,0 +1,16 @@
+SELECT
+    id,
+    season,
+    round,
+    race_name,
+    race_date,
+    driver_id,
+    driver_name,
+    constructor_id,
+    constructor_name,
+    grid_position,
+    finish_position,
+    points,
+    laps,
+    status
+FROM public.race_results
