8c6f906ebaaef57bba21949c0df94961052bcf74
1NVI5IBLE <62433063+1NVI5IBLE@users.noreply.github.com>
2026-09-14 09:42:46 +0100

    Save raw F1 API data as JSON

--- src/extract.py
+++ src/extract.py
@@ -17,4 +17,11 @@ data = response.json()
 
 print("Data extracted successfully!")
 
-print(data)
+
+
+output_path = Path("data") / "race_results_2025.json"
+
+with open(output_path, "w", encoding="utf-8") as file:
+    json.dump(data, file, indent=4)
+
+print (f"Data saved to {output_path}")
